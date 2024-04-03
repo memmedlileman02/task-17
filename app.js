9d06bd95492f6b6cb74d04871e6bd531f2d86d41
memmedlileman <memmedileman02@gmail.com>
2024-04-03 14:00:30 +0400

    task17

---
+++ app.js
@@ -0,0 +1,56 @@
+// let products = [
+//   {
+//     id: 1,
+//     name: "Iphone 14 Pro",
+//     price: 900,
+//   },
+//   {
+//     id: 2,
+//     name: "BMW",
+//     price: 20000,
+//   },
+//   {
+//     id: 3,
+//     name: "Samsung A70",
+//     price: 600,
+//   },
+//   {
+//     id: 4,
+//     name: "Iphone 15 Pro Max", 
+//     price: 1500,
+//   },
+// ];
+// console.log(products);
+// let search = prompt("Search: ").toLowerCase(); //Ip = ip
+// let bosArray = [];
+// let productFilter;
+// // products.map((product)=> console.log(product.name))
+// if (search === "baha") {
+//   productFilter = products.filter((product) => product.price > 1000);
+// } else if (search === "ucuz") {
+//   productFilter = products.filter((product) => product.price < 1000);
+// } else {
+//   productFilter = products.filter((product) => {
+//     product.name.toLowerCase().includes(search);
+//   });
+// }
+// let productFilter = products.sort((a,b) => a.price-b.price);
+// console.log(productFilter);
+
+
+
+
+
+
+// let array = [];
+// for(let i = 0; i < 5; i++){
+//     let random = Math.floor(Math.random() * 100);
+//     array.push(random); //unshift de yazsam isleyir
+// }
+// console.log("array:", array);
+
+// let sum = 0;
+// for(let i = 0; i < array.length; i++){
+//     sum += array[i];
+// }
+// console.log("cem:", sum);
